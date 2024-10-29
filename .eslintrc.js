fd7558f0510ec7e70fd59de333fc79ad8671c841
John Brunton <1276413+jbrunton@users.noreply.github.com>
2024-10-29 22:17:10 +0000

    chore: configure eslint external boundaries

--- .eslintrc.js
+++ .eslintrc.js
@@ -80,5 +80,26 @@ module.exports = {
         ],
       },
     ],
+    "boundaries/external": [
+      2,
+      {
+        // disallow all external imports by default
+        default: "disallow",
+        rules: [
+          {
+            from: "*",
+            allow: ["remeda", "seedrandom"],
+          },
+          {
+            from: "app",
+            allow: "*",
+          },
+          {
+            from: ["data", "migrations"],
+            allow: "knex",
+          },
+        ],
+      },
+    ],
   },
 };
