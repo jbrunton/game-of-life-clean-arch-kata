bb4112972fbac9e9f53a9f0da96d459042b09df1
John Brunton <1276413+jbrunton@users.noreply.github.com>
2024-11-02 22:59:29 +0000

    refactor: yargs

---
+++ src/app/commands/save.ts
@@ -0,0 +1,58 @@
+import { getInitialBoard } from "app/input";
+import { saveGame } from "data/save";
+import { Game } from "entities/game";
+import { Arguments, Argv, CommandModule, InferredOptionTypes } from "yargs";
+
+const args = {
+  width: {
+    type: "number",
+    alias: "w",
+    default: 10,
+    describe: "board width",
+  },
+  height: {
+    type: "number",
+    alias: "h",
+    default: 10,
+    describe: "board height",
+  },
+  seed: { type: "string", alias: "s" },
+  "cell-count": {
+    type: "number",
+  },
+  description: {
+    type: "string",
+    alias: "d",
+    describe: "a description of the saved game",
+  },
+  name: {
+    type: "string",
+    alias: "n",
+    describe: "saved game name",
+    demandOption: true,
+  },
+} as const;
+
+type ArgsT = Arguments<InferredOptionTypes<typeof args>>;
+
+export const saveCommand: CommandModule<object, ArgsT> = {
+  command: "save",
+  describe: "save a game to replay",
+  builder: (yargs: Argv) => yargs.options(args),
+  handler: async (args) => {
+    const width = args.width;
+    const height = args.height;
+
+    const game =
+      args.seed && args.cellCount
+        ? Game.seed({
+            width,
+            height,
+            seed: args.seed,
+            cellCount: args.cellCount,
+          })
+        : await getInitialBoard(width, height);
+
+    await saveGame(args.name, game, args.description);
+  },
+};
--- src/app/main.ts
+++ src/app/main.ts
@@ -1,99 +1,96 @@
-import { Game } from "entities/game";
-import { playGame } from "./play";
-import { getInitialBoard } from "./input";
-import { parseNumber } from "./commands/parsers";
-import { Command } from "commander";
-import { getSavedGame, listGames, saveGame } from "data/save";
 import { db } from "data/db";
-import { pick } from "remeda";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { listCommand } from "./commands/list";
+import { saveCommand } from "./commands/save";
 
-const program = new Command("game-of-life");
+// program
+//   .command("save")
+//   .option("-w, --width <number>", "board width", parseNumber, 10)
+//   .option("-h, --height <number>", "board height", parseNumber, 10)
+//   .option("-s, --seed <number>", "seed value")
+//   .option("-c, --cell-count <number>", "number of live cells", parseNumber)
+//   .option("-d, --description <string>", "a description of the saved game")
+//   .requiredOption("-n, --name <string>", "saved game name")
+//   .action(async (opts) => {
+//     const width = opts.width;
+//     const height = opts.height;
 
-program
-  .command("save")
-  .option("-w, --width <number>", "board width", parseNumber, 10)
-  .option("-h, --height <number>", "board height", parseNumber, 10)
-  .option("-s, --seed <number>", "seed value")
-  .option("-c, --cell-count <number>", "number of live cells", parseNumber)
-  .option("-d, --description <string>", "a description of the saved game")
-  .requiredOption("-n, --name <string>", "saved game name")
-  .action(async (opts) => {
-    const width = opts.width;
-    const height = opts.height;
+//     const game =
+//       opts.seed && opts.cellCount
+//         ? Game.seed({
+//             width,
+//             height,
+//             seed: opts.seed,
+//             cellCount: opts.cellCount,
+//           })
+//         : await getInitialBoard(width, height);
 
-    const game =
-      opts.seed && opts.cellCount
-        ? Game.seed({
-            width,
-            height,
-            seed: opts.seed,
-            cellCount: opts.cellCount,
-          })
-        : await getInitialBoard(width, height);
+//     await saveGame(opts.name, game, opts.description);
+//   });
 
-    await saveGame(opts.name, game, opts.description);
-  });
+// program.command("list").action(async () => {
+//   const games = await listGames();
+//   console.table(games.map((game) => pick(game, ["name", "description"])));
+// });
 
-program.command("list").action(async () => {
-  const games = await listGames();
-  console.table(games.map((game) => pick(game, ["name", "description"])));
-});
+// program
+//   .command("play")
+//   .option("-w, --width <number>", "board width", parseNumber, 10)
+//   .option("-h, --height <number>", "board height", parseNumber, 10)
+//   .option("-s, --seed <number>", "seed value")
+//   .option("-c, --cell-count <number>", "number of live cells", parseNumber)
+//   .option("-t, --max-turns <number>", "max turns to play", parseNumber, 50)
+//   .option(
+//     "-d, --delay <number>",
+//     "delay per rendered frame, ms",
+//     parseNumber,
+//     50,
+//   )
+//   .option("-q, --quiet", "skips rendering the output")
+//   .option("-a, --print-all", "print all turns (disable animation)")
+//   .option("-l, --loop", "don't exit")
+//   .option("-n, --name <string>", "play a saved game")
+//   .action(async (opts) => {
+//     const width = opts.width;
+//     const height = opts.height;
 
-program
-  .command("play")
-  .option("-w, --width <number>", "board width", parseNumber, 10)
-  .option("-h, --height <number>", "board height", parseNumber, 10)
-  .option("-s, --seed <number>", "seed value")
-  .option("-c, --cell-count <number>", "number of live cells", parseNumber)
-  .option("-t, --max-turns <number>", "max turns to play", parseNumber, 50)
-  .option(
-    "-d, --delay <number>",
-    "delay per rendered frame, ms",
-    parseNumber,
-    50,
-  )
-  .option("-q, --quiet", "skips rendering the output")
-  .option("-a, --print-all", "print all turns (disable animation)")
-  .option("-l, --loop", "don't exit")
-  .option("-n, --name <string>", "play a saved game")
-  .action(async (opts) => {
-    const width = opts.width;
-    const height = opts.height;
+//     if (
+//       width * 2 > process.stdout.columns ||
+//       height + 1 > process.stdout.rows
+//     ) {
+//       console.error(
+//         `Console is too small (${process.stdout.columns / 2} x ${
+//           process.stdout.rows
+//         }) for specified dimensions (${width} x ${height}). Note: grid cells occupy two terminal columns.`,
+//       );
+//       process.exit(1);
+//     }
 
-    if (
-      width * 2 > process.stdout.columns ||
-      height + 1 > process.stdout.rows
-    ) {
-      console.error(
-        `Console is too small (${process.stdout.columns / 2} x ${
-          process.stdout.rows
-        }) for specified dimensions (${width} x ${height}). Note: grid cells occupy two terminal columns.`,
-      );
-      process.exit(1);
-    }
+//     const quiet = opts.quiet ?? false;
+//     const loop = opts.loop ?? false;
+//     const printAll = opts.printAll ?? false;
+//     const maxTurns = opts.maxTurns;
+//     const delayMs = opts.delay;
+//     const name = opts.name;
 
-    const quiet = opts.quiet ?? false;
-    const loop = opts.loop ?? false;
-    const printAll = opts.printAll ?? false;
-    const maxTurns = opts.maxTurns;
-    const delayMs = opts.delay;
-    const name = opts.name;
+//     const game = opts.name
+//       ? await getSavedGame(name)
+//       : opts.seed && opts.cellCount
+//         ? Game.seed({
+//             width,
+//             height,
+//             seed: opts.seed,
+//             cellCount: opts.cellCount,
+//           })
+//         : await getInitialBoard(width, height);
 
-    const game = opts.name
-      ? await getSavedGame(name)
-      : opts.seed && opts.cellCount
-        ? Game.seed({
-            width,
-            height,
-            seed: opts.seed,
-            cellCount: opts.cellCount,
-          })
-        : await getInitialBoard(width, height);
+//     await playGame(game, { maxTurns, delayMs, quiet, loop, printAll });
+//   });
 
-    await playGame(game, { maxTurns, delayMs, quiet, loop, printAll });
-  });
-
-program
+yargs(hideBin(process.argv))
+  .command(listCommand)
+  .command(saveCommand)
   .parseAsync()
   .then(() => db.destroy())
   .catch((e) => {
