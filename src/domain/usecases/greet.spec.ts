ffc12edb0b526b21a0aebb543cb6a87dbcecb4cc
John Brunton <1276413+jbrunton@users.noreply.github.com>
2025-04-15 08:49:52 +0100

    refactor: remove unused greet domain code

--- src/domain/usecases/greet.spec.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, it } from "@jest/globals";
-import { getGreeting } from "./greet";
-describe("getGreet", () => {
-  it("returns a greeting", () => {
-    expect(getGreeting({ name: "World" })).toEqual("Hello, World!");
-  });
-});
